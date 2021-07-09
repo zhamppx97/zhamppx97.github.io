ef5f911cf246d89ac6dcca6cb30cc3b269f5572a
Woraphon Khuarprakopkit <zhamppx.wrp@gmail.com>
2021-07-09 13:22:35 +0700

    Update

---
+++ particles.js
@@ -0,0 +1,49 @@
+{
+    particlesJS('particles',
+        {
+            "particles": {
+                "number": {
+                    "value": 50,
+                    "density": {
+                        "enable": true,
+                        "value_area": 800
+                    }
+                },
+                "opacity": {
+                    "value": 0.5,
+                    "random": false
+                },
+                "size": {
+                    "value": 3,
+                    "random": true
+                },
+                "line_linked": {
+                    "enable": true,
+                    "distance": 150,
+                    "color": "#ffffff",
+                    "opacity": 0.4,
+                    "width": 1
+                },
+                "move": {
+                    "enable": true,
+                    "speed": 3,
+                    "direction": "none",
+                    "random": false,
+                    "straight": false,
+                    "out_mode": "out"
+                }
+            },
+            "interactivity": {
+                "events": {
+                    "onhover": {
+                        "enable": false,
+                    },
+                    "onclick": {
+                        "enable": false,
+                    }
+                }
+            },
+            "retina_detect": true
+        }
+    );
+}
